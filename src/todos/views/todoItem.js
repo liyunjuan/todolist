62f8588ec1cd738fd8d4e49adb23439c81da7a8b
yjli <yjli@hillinsight.com>
2020-08-09 19:33:32 +0800

    改造todoItem提高性能

--- src/todos/views/todoItem.js
+++ src/todos/views/todoItem.js
@@ -1,23 +1,43 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { toggleTodo, removeTodo } from '../actions';
 
-const TodoItem = ({ onToggle, onRemove, completed, text }) => (
-    <li 
-        className="todo-item"
-        style={{
-            textDecoration: completed ? 'line-through' : 'none'
-        }}
-    >
-        <input className='toggle' type='checkbox' checked={completed ? 'checked' : ''} readOnly onClick={onToggle} />
-        <label className='text'>{text}</label>
-        <button className='remove' onClick={onRemove}>x</button>
-    </li>
-);
+class TodoItem extends React.Component{
+    constructor() {
+        super(...arguments)
+        console.log('enter TodoItem constructor: ' + this.props.text);
+    }
+
+    render(){
+        const { onToggleTodo, onRemoveTodo, completed, text } = this.props;
+        console.log('enter TodoItem render: ' + text);
+        return (
+            <li 
+                className="todo-item"
+                style={{
+                    textDecoration: completed ? 'line-through' : 'none'
+                }}
+            >
+                <input className='toggle' type='checkbox' checked={completed ? 'checked' : ''} readOnly onClick={onToggleTodo} />
+                <label className='text'>{text}</label>
+                <button className='remove' onClick={onRemoveTodo}>x</button>
+            </li>
+        )
+    }
+}
 
 TodoItem.propTypes = {
-    onToggle: PropTypes.func.isRequired,
-    onRemove: PropTypes.func.isRequired,
+    onToggleTodo: PropTypes.func.isRequired,
+    onRemoveTodo: PropTypes.func.isRequired,
     completed: PropTypes.bool.isRequired,
     text: PropTypes.string.isRequired,
 }
 
-export default TodoItem;
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        onToggleTodo: () => dispatch(toggleTodo(ownProps.id)),
+        onRemoveTodo: () => dispatch(removeTodo(ownProps.id))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(TodoItem);
--- src/todos/views/todoList.js
+++ src/todos/views/todoList.js
@@ -1,7 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import TodoItem from './todoItem.js'
-import { toggleTodo, removeTodo } from '../actions';
 import { FilterTypes } from '../../constants.js'
 
 const TodoList = ({ todos, onToggleTodo, onRemoveTodo }) => {
@@ -13,8 +12,7 @@ const TodoList = ({ todos, onToggleTodo, onRemoveTodo }) => {
                         key={item.id}
                         text={item.text}
                         completed={item.completed}
-                        onToggle={() => onToggleTodo(item.id)}
-                        onRemove={() => onRemoveTodo(item.id)}
+                        id={item.id}
                     />
                 ))
             }
@@ -45,15 +43,6 @@ const mapStateToProps = (state) => {
     }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        onToggleTodo: (id) => {
-            dispatch(toggleTodo(id));
-        },
-        onRemoveTodo: (id) => {
-            dispatch(removeTodo(id));
-        }
-    }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
+
+export default connect(mapStateToProps, null)(TodoList);
